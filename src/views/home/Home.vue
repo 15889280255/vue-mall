cf25b3afefa08a12423381471a0c7cb2b6a1a387
xujinfeng <1352176165@qq.com>
2020-06-30 21:02:52 +0800

    添加首页轮播图和推荐

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,14 +1,45 @@
 <template>
   <div>
-    <h2>首页</h2>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <home-swiper :banners="banner"/>
+    <home-recommend :recommends="recommend"/>
   </div>
 </template>
 
 <script>
+import NavBar from "components/common/navbar/NavBar.vue";
+import HomeSwiper from './childComps/HomeSwiper'
+import HomeRecommend from './childComps/HomeRecommend'
+
+import { getHomeMultidata } from "network/home";
+
 export default {
-  name:"home"
-}
+  name: "home",
+  components: {
+    NavBar,
+    HomeSwiper,
+    HomeRecommend,
+  },
+  data() {
+    return {
+      banner: [],
+      recommend: []
+    };
+  },
+  created() {
+    getHomeMultidata().then(val => {
+      this.banner = val.data.banner.list;
+      this.recommend = val.data.recommend.list;
+    });
+  }
+};
 </script>
 
 <style>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
---
+++ src/views/home/childComps/HomeRecommend.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="recommend">
+    <div v-for="item in recommends" :key="item.index" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props:{
+    recommends:{
+      type:Array,
+      default(){
+        return [];
+      }
+    }
+  }
+}
+</script>
+
+<style>
+  .recommend{
+    display: flex;
+    text-align: center;
+    font-size: 12px;
+    padding: 16px 5px;
+    border-bottom: 8px solid #eee;
+  }
+
+  .recommend-item{
+    flex: 1;
+  }
+
+  .recommend-item img{
+    width: 70%;
+    margin-bottom: 5px;
+  }
+
+
+</style>
