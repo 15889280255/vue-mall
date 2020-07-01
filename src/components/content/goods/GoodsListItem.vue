833c0427de5cf9855642f0381ebb71ba8fb3af1b
xujinfeng <1352176165@qq.com>
2020-07-01 09:23:50 +0800

    首页商品展示

---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="goods-list-item">
+    <a :href="item.link">
+      <img :src="item.show.img" alt class="item-img" />
+      <p class="item-title">{{item.title}}</p>
+      <span class="item-price">{{item.price}}￥</span>
+      <span class="item-cfav">- {{item.cfav}}人收藏</span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  }
+};
+</script>
+
+<style>
+.goods-list-item {
+  padding-bottom: 10px;
+  position: relative;
+  font-size: 12px;
+  width: 47%;
+  text-align: center;
+}
+
+.item-img {
+  width: 100%;
+  border-radius: 5px;
+}
+
+.item-title {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap; /**指定文本不换行 */
+  margin-bottom: 4px;
+}
+
+.item-price {
+  color: var(--color-high-text);
+}
+
+.item-cfav {
+  color: var(--color-high-text);
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,17 +3,24 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-    <home-swiper :banners="banner"/>
-    <home-recommend :recommends="recommend"/>
+    <home-swiper :banners="banner" />
+    <home-recommend :recommends="recommend" />
+    <future-view />
+    <tab-control :titles="['流行','精选','新款']" class="tab-control"/>
+    <goods-list :goods="goods['pop'].list"/>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar.vue";
-import HomeSwiper from './childComps/HomeSwiper'
-import HomeRecommend from './childComps/HomeRecommend'
+import HomeSwiper from "./childComps/HomeSwiper";
+import HomeRecommend from "./childComps/HomeRecommend";
+import FutureView from "./childComps/FutureView";
+import GoodsList from "content/goods/GoodsList";
 
-import { getHomeMultidata } from "network/home";
+import TabControl from "content/tabControl/TabControl";
+
+import { getHomeMultidata, getHomeData } from "network/home";
 
 export default {
   name: "home",
@@ -21,18 +28,41 @@ export default {
     NavBar,
     HomeSwiper,
     HomeRecommend,
+    FutureView,
+    TabControl,
+    GoodsList
   },
   data() {
     return {
       banner: [],
-      recommend: []
+      recommend: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] }
+      }
     };
   },
   created() {
-    getHomeMultidata().then(val => {
-      this.banner = val.data.banner.list;
-      this.recommend = val.data.recommend.list;
-    });
+    this.getHomeMultidata();
+    this.getHomeData("pop", 0);
+    this.getHomeData("new", 0);
+    this.getHomeData("sell", 0);
+  },
+  methods: {
+    getHomeMultidata() {
+      getHomeMultidata().then(val => {
+        this.banner = val.data.banner.list;
+        this.recommend = val.data.recommend.list;
+      });
+    },
+    getHomeData(type) {
+      const page = this.goods[type].page + 1;
+      getHomeData(type, page).then(val => {
+        this.goods[type].list.push(...val.data.list)
+        this.goods[type].page +=1;
+      });
+    }
   }
 };
 </script>
@@ -41,5 +71,14 @@ export default {
 .home-nav {
   background-color: var(--color-tint);
   color: #fff;
+  position: sticky;
+  top: 0px;
+  z-index: 100;
+}
+
+.tab-control{
+  position: sticky;
+  top: 49px;
+  z-index: 10;
 }
 </style>
