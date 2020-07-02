69a15675d3afa11ba7c04bc5da87e5e337d1c5a0
xujinfeng <1352176165@qq.com>
2020-07-02 23:04:17 +0800

    详细页请求

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -5,25 +5,35 @@
 </template>
 
 <script>
-import DetailNavBar from './childComps/DetailNavBar'
+import DetailNavBar from "./childComps/DetailNavBar";
+
+import { getDetail } from "network/detail";
 export default {
-  data(){
-    return{
-      iid:null,
-    }
+  data() {
+    return {
+      iid: null,
+      topImages:[]
+    };
   },
-  components:{
-    DetailNavBar,
+  components: {
+    DetailNavBar
   },
   created() {
-    this.iid = this.$route.params.iid;
-    console.log(this.iid);
-    
+    this.getDetail();
   },
+  methods: {
+    getDetail() {
+      this.iid = this.$route.params.iid;
+      getDetail(this.iid).then(res => {
+        const data = res.result;
+        console.log(data);
 
-}
+        this.topImages = data.itemInfo.topImages
+      });
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
