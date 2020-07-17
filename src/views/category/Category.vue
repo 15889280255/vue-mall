97eaf14ccee1abdce327566b98be552dfbfef020
xujinfeng <1352176165@qq.com>
2020-07-17 22:19:57 +0800

    分类搜索框

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
-    <h2>分类</h2>
+    <search-bar @searchClick="searchClick"></search-bar>
   </div>
 </template>
 
 <script>
+import SearchBar from './childComps/SearchBar'
 export default {
-  name:"category"
-}
+  name: "category",
+  components: {
+    SearchBar,
+  },
+  methods:{
+    searchClick(searchData){
+      console.log('搜索:'+ searchData);
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
---
+++ src/views/category/childComps/SearchBar.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <nav-bar>
+      <div slot="left" class="left">
+        <img src="~assets/img/common/follow_note.png" alt />
+      </div>
+      <div slot="center" class="center">
+        <input type="text" v-model="searchData" @focus="focus" @blur="blur" />
+      </div>
+      <div slot="right" class="right" @click="searchClick">
+        <img src="~assets/img/common/search.png" alt />
+      </div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar.vue";
+export default {
+  data() {
+    return {
+      searchData: "搜索商品"
+    };
+  },
+  components: {
+    NavBar
+  },
+  methods: {
+    searchClick() {
+      this.$emit("searchClick", this.searchData);
+    },
+    focus() {
+      this.searchData = "";
+      console.log('aaaaaaaaaaaa');
+      
+    },
+    blur() {
+      console.log('失去焦点事件blur');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.center input {
+  border-radius: 15px;
+  outline: none;
+  height: 30px;
+  padding-left: 5px;
+  width: 80%;
+  border-color: white;
+  font-size: 13px;
+  color: grey;
+}
+
+.left,
+.right,
+.center {
+  border: 1px solid white;
+  background-color: var(--color-tint);
+}
+
+.left img,
+.right img {
+  width: 22px;
+  height: 22px;
+  padding-top: 5px;
+}
+</style>
