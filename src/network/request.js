309543a091b6583520e9675250722b702b1f254f
xujinfeng <1352176165@qq.com>
2020-08-04 15:26:35 +0800

    修改gitignore

--- src/network/request.js
+++ src/network/request.js
@@ -1,17 +1,18 @@
 import axios from 'axios'
+import baseURL from '../no_git'
 
-export function request(config){
+export function request(config) {
   const instance = axios.create({
-    baseURL:"http://152.136.185.210:8000/api/n3",
-    timeout:5000
+    baseURL: baseURL,
+    timeout: 5000
   })
 
-  instance.interceptors.request.use(config=>{
+  instance.interceptors.request.use(config => {
     console.log(config);
     return config;
   })
 
-  instance.interceptors.response.use(res=>{
+  instance.interceptors.response.use(res => {
     return res.data;
   })
 
